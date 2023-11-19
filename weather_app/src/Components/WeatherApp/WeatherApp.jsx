a08e999bb16a42c2f7254493832180db27bdd537
hudson254 <hudsonwainaina@gmail.com>
2023-11-20 00:26:47 +0300

    icon updating error

--- weather_app/src/Components/WeatherApp/WeatherApp.jsx
+++ weather_app/src/Components/WeatherApp/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './WeatherApp.css'
 
 import search_icon from "../Assets/search.png";
@@ -13,6 +13,8 @@ export const WeatherApp = () => {
 
     let api_key ="cc1cb684182004c35dc01b0d55487770";
 
+    const [wicon,setWicon] = useState (cloud_icon);
+    
     const search = async () => {
         const element =document.getElementsByClassName("cityInput")
         if (element[0].value==="")
@@ -30,9 +32,42 @@ export const WeatherApp = () => {
         const location = document.getElementsByClassName("weather-location")
 
         humidity[0].innerHTML = data.main.humidity+" %";
-        wind[0].innerHTML = data.wind.speed+" Km/h";
-        temperature[0].innerHTML = data.main.temp+" ⁰C";
+        wind[0].innerHTML = Math.floor(data.wind.speed)+" Km/h";
+        temperature[0].innerHTML = Math.floor(data.main.temp)+" ⁰C";
         location[0].innerHTML = data.name;
+
+        if(data.weather[0].icon==="01d" || data.weather[0].icon==="01n")
+        {
+            setWicon(clear_icon);
+        }
+        else if(data.weather[0].icon==="02d" || data.weather[0].icon==="02n")
+        {
+            setWicon(cloud_icon);
+        }
+        else if(data.weather[0].icon==="03d" || data.weather[0].icon==="03n")
+        {
+            setWicon(drizzle_icon);
+        }
+        else if(data.weather[0].icon==="04d" || data.weather[0].icon==="04n")
+        {
+            setWicon(drizzle_icon);
+        }
+        else if(data.weather[0].icon==="09d" || data.weather[0].icon==="09n")
+        {
+            setWicon(rain_icon);
+        }
+        else if(data.weather[0].icon==="10d" || data.weather[0].icon==="10n")
+        {
+            setWicon(rain_icon);
+        }
+        else if(data.weather[0].icon==="13d" || data.weather[0].icon==="13n")
+        {
+            setWicon(snow_icon);
+        }
+        else 
+        {
+            setWicon(clear_icon);
+        }
     }
 
   return (
